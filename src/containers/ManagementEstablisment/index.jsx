17d589c3d532ae769acfadf51f4abc02b4fa5af3
KITTITORN_Pin <kttpy.game@gmail.com>
2024-03-24 20:29:05 +0700

    fix

---
+++ src/containers/ManagementEstablisment/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ManagementEstablisment = () => {
+  return (
+    <div>
+      หน้าการจัดตั้งนิติบุลคลหมู่บ้านจัดสรร
+    </div>
+  )
+}
+
+export default ManagementEstablisment
---
+++ src/pages/ManagementCondominium/index.js
@@ -0,0 +1,10 @@
+import ManagementCondominium from '@/containers/ManagementCondominium'
+import React from 'react'
+
+const index = () => {
+  return (
+    <ManagementCondominium/>
+  )
+}
+
+export default index
