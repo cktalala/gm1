4d065fb3a27ace599806d41a9afcfad6ddb49394
KITTITORN_Pin <kttpy.game@gmail.com>
2024-03-24 19:03:57 +0700

    update

--- src/containers/Login/index.jsx
+++ src/containers/Login/index.jsx
@@ -10,9 +10,11 @@ const LoginPage = () => {
     <>
       <div className="h-screen flex justify-center items-center">
         <div className="w-[300px]  bg-[#eaeaea] shadow-md rounded-lg p-20">
-          <h1>ระบบตรวจติดตามงาน </h1>
-          <Button type="primary">Primary Button</Button>
-
+          <h1>ระบบตรวจติดตามงาน</h1>
+          <div className="m-10 flex flex-col gap-1">
+          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px rounded-lg ">Button</button>
+          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px rounded-lg">Button</button>
+          </div>
         </div>
       </div>
     </>
